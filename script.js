deb1c7ccd5b4e4c16390e155de5fd013b192d021
ShopifyDev123 <155445127+ShopifyDev123@users.noreply.github.com>
2024-01-30 22:23:56 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -31,26 +31,6 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-
-  // Image Carousel
-let currentIndex = 0;
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % document.querySelectorAll('.carousel-slide').length;
-  updateCarousel();
-}
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + document.querySelectorAll('.carousel-slide').length) % document.querySelectorAll('.carousel-slide').length;
-  updateCarousel();
-}
-
-function updateCarousel() {
-  const container = document.querySelector('.carousel-container');
-  const slideWidth = document.querySelector('.carousel-slide').offsetWidth;
-  container.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
-}
-
 // Mobile Menu
 
 function toggleMobileMenu() {
@@ -97,3 +77,35 @@ document.addEventListener('DOMContentLoaded', function () {
     alert('Submission successful!'); // You can customize this alert message
     // Add additional actions or modifications to your page as needed
   }
+
+// Slide
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slides")[0].getElementsByTagName("img");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+}
